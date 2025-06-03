4c65eb3826bda94d79ca98dc829dfca37918ed27
suryakumar120 <gsuryakumar2to2.5@gmail.com>
2025-06-04 00:33:42 +0530

    changed cron to run once every hour

--- server/jobs/cron.ts
+++ server/jobs/cron.ts
@@ -5,7 +5,7 @@ import { processPendingRedemptions } from '../services/redemptionService';
 export function startCronJobs() {
   console.log('Setting up cron jobs', 'cron');
 
-  cron.schedule('* * * * *', async () => {
+  cron.schedule('0 * * * *', async () => {
     console.log('Running scheduled redemption processing', 'cron');
     try {
       const result = await processPendingRedemptions();
